5c0da6e55f6f6b09de38652988d18260fca824e2
adamd <adam.d.hinton@gmail.com>
2024-04-26 10:18:13 -0400

    notes

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Threads are fundamental building blocks of concurrency
+        // Perform mult tasks simul within a single process
+        // We've been using a thread - the main thread - all along. JVM fires this up every time we use main method
+
+        Thread currentThread = Thread.currentThread();
+        System.out.println(currentThread.getClass().getName());
+
+
+        // name
+        // group ThreadGroup
+        // priority int
+        // target Runnable (run())
+//        private tid int
+        // run()
+        // start()
+
     }
 }
